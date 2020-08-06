c3e5b0862ffe349697f3d74f4bbddf95769b125c
Paula <paula.batoon@gmail.com@users.noreply.github.com>
2020-08-06 15:06:31 -0500

    added number of reps and rep type

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-let exercises = ["shadowboxing", "1 mile run", "mitt work", "bag work", "sprints"];
+let exercises = ["shadowboxing", "run", "mitt work", "bag work", "sprints", "squats"];
+let repTypes = ["reps","miles", "rounds"];
+
 let exerciseSelect = document.getElementById('exerciseSelect');
 let exerciseList = document.getElementById('exerciseList');
+let repCount = document.getElementById('repCount');
+let repType = document.getElementById('repType');
 
  
 function populateDropdown() {
@@ -11,15 +15,29 @@ function populateDropdown() {
         el.value = option;
         exerciseSelect.appendChild(el);
     }
+    for (i in repTypes) {
+        let option = repTypes[i];
+        let el = document.createElement('option');
+
+        el.textContent = option;
+        el.value = option;
+
+        repType.appendChild(el);
+    }
 }
 
 
 function addExercise(){
-    let exercise = exerciseSelect.value;
     let entry = document.createElement('li');
+    let exercise = exerciseSelect.value;
+    let numReps = repCount.value;
+    let rep = repType.value;
 
-    entry.appendChild(document.createTextNode(exercise));
-    exerciseList.appendChild(entry);
+    if(exerciseList.selectedIndex != 0 && repType.selectedIndex != 0) {
+        let returnString = exercise + " x " + numReps + " " + rep;
+        entry.appendChild(document.createTextNode(returnString));
+        exerciseList.appendChild(entry);
+    }
 }
 
 
